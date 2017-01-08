756d93f5ef8c7bed1a1905c286913ba42b15b19a
fzred <1179750402@qq.com>
2017-01-08 14:40:08 +0800

    update

--- src/models/Commit.ts
+++ src/models/Commit.ts
@@ -2,9 +2,15 @@
  * 提交
  */
 interface Commit {
-  verNumber: string,
-  repoName: string,
+  readonly verNumber: string, // 要同步的版本
+  readonly repoName: string,  // 仓库名
+  syncRepoList: Array<ayncRepo>,  // 要同步到的其他仓库
+}
 
+interface ayncRepo {
+  synced: boolean, // 是否已同步
+  readonly repoName: string,  // 同步的仓库名
+  syncTime?: Date,  // 同步时间
 }
 
 export default Commit
--- src/models/Repository.ts
+++ src/models/Repository.ts
@@ -2,8 +2,8 @@
  * 仓库
  */
 interface Repository {
-  name: '',
-  url: '',
+  readonly name: '',
+  readonly url: '',
 }
 
 export default Repository
