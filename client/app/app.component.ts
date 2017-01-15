b2a3e90b6f7956d244c20c6ce85f7c5faae5eedf
fzred <1179750402@qq.com>
2017-01-15 23:24:27 +0800

    update

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -3,14 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: __uri('./app.component.html'),
-//   template: `
-// <h1>Hello {{title}}</h1>
-// <ul>
-//       <li *ngFor="let hero of commints">
-//         {{ hero }}
-//       </li>
-//     </ul>
-// `,
 })
 export class AppComponent {
   title: string;
