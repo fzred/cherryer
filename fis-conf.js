da13d1c9a8cdf45616770d118d5d6982a25e051d
fzred <1179750402@qq.com>
2017-01-15 20:15:24 +0800

    update

--- fis-conf.js
+++ fis-conf.js
@@ -1,3 +1,13 @@
+fis.config.set('project.ignore', [
+  'output/**',
+  'dist/**',
+  'node_modules/**',
+  '.git/**',
+  'fis-conf.js',
+  'npm-debug.log',
+  'db.json',
+])
+
 fis.match('**.ts', {
   parser: fis.plugin('typescript', {
     module: 'commonjs',
