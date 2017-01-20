fc27c9813502e54b5c018a209ca519a5217066b0
fzred <1179750402@qq.com>
2017-01-20 10:18:07 +0800

    update

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,6 +1,7 @@
 import * as path from 'path'
 import * as Lowdb from 'lowdb'
 import Commit from '../models/Commit'
+import Repository from "../models/Repository";
 export const db = new Lowdb(path.resolve(__dirname, '../../db.json'))
 
 
@@ -13,7 +14,18 @@ export function getState() {
 }
 
 export function insertCommit(commit: Commit) {
-  if(db.get('commit').value<Array<Commit>>().find(item => item.verNumber === commit.verNumber))
+  if (db.get('commit').value<Array<Commit>>().find(item => item.verNumber === commit.verNumber))
     throw new Error('verNumber重复')
   return db.get('commit').push(commit).value()
 }
+
+export function getRepositoryList() {
+  return db.get('repository').value()
+}
+
+export function insertRepository(repository: Repository) {
+  if (db.get('repository').value<Array<Repository>>().find(item => item.name === repository.name))
+    throw new Error('name重复')
+  return db.get('repository').push(repository).value()
+}
+
