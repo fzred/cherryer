e2285c0831523e655d62439a54197d23aebf3a73
fzred <1179750402@qq.com>
2017-01-20 14:54:26 +0800

    update

--- server/db/db.ts
+++ server/db/db.ts
@@ -5,25 +5,51 @@ import Repository from "../models/Repository";
 export const db = new Lowdb(path.resolve(__dirname, '../../db.json'))
 
 
-export function getCommitList() {
+export function getCommitList () {
   return db.get('commit').value()
 }
 
-export function getState() {
+export function getCommitByVerNumber (verNumber): Commit {
+  return db.get('commit').find({ verNumber }).value() as Commit
+}
+
+export function getState () {
   return db.getState()
 }
 
-export function insertCommit(commit: Commit) {
+export function insertCommit (commit: Commit) {
   if (db.get('commit').value<Array<Commit>>().find(item => item.verNumber === commit.verNumber))
     throw new Error('verNumber重复')
   return db.get('commit').push(commit).value()
 }
 
-export function getRepositoryList() {
+// export function updateCommitSyncRepo (verNumber: string, repoName: string) {
+//   return db.get('commit')
+//     .find({ verNumber })
+//     .find(repoName)
+//     .assign({ synced: true })
+//     .value()
+// }
+
+export function updateCommit (verNumber: string, commit: Commit) {
+  return db.get('commit')
+    .find({ verNumber })
+    .assign(commit)
+    .value()
+}
+
+export function updateCommitSyncRepoList (verNumber: string, syncRepoList: AyncRepo[]) {
+  return db.get('commit')
+    .find({ verNumber })
+    .assign({ syncRepoList })
+    .value()
+}
+
+export function getRepositoryList () {
   return db.get('repository').value()
 }
 
-export function insertRepository(repository: Repository) {
+export function insertRepository (repository: Repository) {
   if (db.get('repository').value<Array<Repository>>().find(item => item.name === repository.name))
     throw new Error('name重复')
   return db.get('repository').push(repository).value()
--- server/router.ts
+++ server/router.ts
@@ -2,7 +2,7 @@ import * as KoaRouter from 'koa-router'
 import * as send from 'koa-send'
 import * as db from'./db/db'
 const router = new KoaRouter()
-import{checkStrEmpty} from './utils'
+import { checkStrEmpty } from './utils'
 
 router.get('/api/getState', async ctx => {
   ctx.body = db.getState()
@@ -13,7 +13,7 @@ router.get('/api/getCommitList', async ctx => {
 })
 
 router.get('/api/insertCommit', async ctx => {
-  const {verNumber, repoName, syncRepoName} = ctx.request.query
+  const { verNumber, repoName, syncRepoName } = ctx.request.query
   if (checkStrEmpty(verNumber)) {
     throw new Error('verNumber必填')
   }
@@ -42,6 +42,60 @@ router.get('/api/insertCommit', async ctx => {
     verNumber,
     repoName,
     syncRepoList,
+  })
+  ctx.body = {
+    code: 1000,
+    desc: 'success',
+    data: null,
+  }
+})
+
+router.get('/api/commitAddSyncRepo', async ctx => {
+  const { verNumber, syncRepoName } = ctx.request.query
+  const commit = db.getCommitByVerNumber(verNumber)
+  commit.syncRepoList.push({
+    synced: false,
+    repoName: syncRepoName,
+  })
+
+  db.updateCommit(verNumber, commit)
+
+  ctx.body = {
+    code: 1000,
+    desc: 'success',
+    data: null,
+  }
+})
+
+router.get('/api/commitUpdateSyncRepo', async ctx => {
+  const { verNumber, syncRepoName, synced } = ctx.request.query
+  const commit = db.getCommitByVerNumber(verNumber)
+  commit.syncRepoList.find(item => item.repoName === syncRepoName).synced = synced
+
+  db.updateCommit(verNumber, commit)
+
+  ctx.body = {
+    code: 1000,
+    desc: 'success',
+    data: null,
+  }
+})
+
+router.get('/api/getRepositoryList', async ctx => {
+  ctx.body = db.getRepositoryList()
+})
+
+router.get('/api/insertRepository', async ctx => {
+  const { name, url } = ctx.request.query
+  if (checkStrEmpty(name)) {
+    throw new Error('name必填')
+  }
+  if (checkStrEmpty(url)) {
+    throw new Error('url必填')
+  }
+  db.insertRepository({
+    name,
+    url,
   })
   ctx.body = {
     code: 1000,
